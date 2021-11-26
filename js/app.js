5163a5c78d8b5e9d0727390ee96603ff1ebcb75a
Agustina Goncalves <agus.goncalves95@gmail.com>
2021-11-26 19:13:48 -0300

    vaciar main

--- js/app.js
+++ js/app.js
@@ -1,17 +1,9 @@
 var app = new Vue({
     el: '#app',
     data: {
-        numeroClick: 0,
-        contador: 0,
    },
     methods: {
-        sumarClick(){
-            this.numeroClick ++
-            this.contador ++
         },
-        restarClick(){
-            this.numeroClick --
-            this.contador ++
         }
     },
     computed: {
